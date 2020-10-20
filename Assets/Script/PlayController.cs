74463b091db806089d6b9401946c1ae524113f04
toby0622 <toby0622@gmail.com>
2020-10-20 11:00:04 +0800

    Update PlayController.cs

--- Assets/Script/PlayController.cs
+++ Assets/Script/PlayController.cs
@@ -13,6 +13,6 @@ public class PlayController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
