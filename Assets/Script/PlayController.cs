876d42347f16642d74bb80acc07fd06582d8682d
TobyTheFool <toby0622@gmail.com>
2020-10-27 11:31:39 +0800

    Refine

--- Assets/Script/PlayController.cs
+++ Assets/Script/PlayController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayController : MonoBehaviour
 {
+    public float speed = 20;
+    public float turnspeed = 20;
+    public float horizontal_input;
+    public float vertical_input;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontal_input = Input.GetAxis("Horizontal");
+        vertical_input = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical_input);
+        // transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontal_input);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontal_input);
     }
 }
